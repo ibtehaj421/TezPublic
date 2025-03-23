503f694df79297fe4bb20e04cf6ab288735e5d09
ibtehaj421 <ibtehajkazmi09@gmail.com>
2025-03-23 17:04:40 +0500

    minor

--- DTOs/Register.cs
+++ DTOs/Register.cs
@@ -5,6 +5,6 @@ public class Register {
 
     public string? role {get; set;} //the admins will get their roles from the organization admin.
     public int level {get; set;} //organization id sent back
-    public long orgid {get; set;} //each org has a unique id.
-
+    public string? orgName {get; set;} //each org has a unique name.
+    public long orgId {get; set;} //each org has a unique id
 }
